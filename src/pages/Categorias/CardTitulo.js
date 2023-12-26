573cec90a65aee7c37bfa12434678272864b5931
dev-brunosantos <brunosflauta@gmail.com>
2023-12-26 16:15:39 -0300

    Alterações na tela de CATEGORIAS

---
+++ src/pages/Categorias/CardTitulo.js
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { Entypo } from '@expo/vector-icons'
+import { CardCategoria } from "../../components/Cards/CardCategoria";
+import { Cores } from "../../styles/Cores";
+
+export default function CardTitulo({imagem, titulo}) {
+
+    const teste = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    function Ver() {
+        return (
+            teste.map(aaa => (
+                <CardCategoria />
+            ))
+        )
+    }
+
+    const [setaAbrir, setSetaAbrir] = useState('chevron-small-down')
+    const [altura, setAltura] = useState(0)
+
+    const abrirCategoria = () => {
+        if (setaAbrir === 'chevron-small-down') {
+            setSetaAbrir('chevron-small-up');
+            setAltura('auto');
+        } else {
+            setSetaAbrir('chevron-small-down');
+            setAltura(0);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.btn} onPress={abrirCategoria}>
+                <View style={styles.containerImagem}>
+                    <Image
+                        style={{ width: '100%', height: '100%' }}
+                        src={imagem}
+                        // src="https://bythiti.com.br/wp-content/uploads/2023/09/IMG_8990.jpg"
+                    />
+                </View>
+                <Text style={{ fontSize: 20 }}> {titulo} </Text>
+                {/* <Text style={{ fontSize: 20 }}> Canecas </Text> */}
+                <Entypo
+                    name={setaAbrir} size={30}
+                    style={styles.seta}
+                />
+            </TouchableOpacity>
+
+            <View style={[styles.containerCategoria, {height: altura}]}>
+
+                <Ver />
+
+            </View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        // borderWidth: 1,
+        // borderColor: 'red',
+        marginTop: 20,
+        marginBottom: -15
+    },
+    btn: {
+        height: 80,
+        // borderWidth: 1,
+        borderRadius: 10,
+        marginBottom: 2,
+        padding: 5,
+        flexDirection: 'row',
+        alignItems: "center",
+        shadowOffset: { width: 0, height: 10 },
+        shadowColor: '#242424',
+        shadowOpacity: 0.5,
+        elevation: 5,
+        backgroundColor: Cores.branco
+    },
+    containerImagem: {
+        width: 80,
+        height: '100%',
+        // borderWidth: 1,
+        borderRadius: 5,
+        overflow: "hidden",
+        borderColor: 'green',
+        marginHorizontal: 10,
+    },
+    seta: {
+        position: 'absolute',
+        right: 15
+    },
+    containerCategoria: {
+        width: '100%',
+        flexDirection: 'row',
+        flexWrap: "wrap",
+        gap: 4,
+        alignItems: "center",
+        // justifyContent: "space-evenly",
+        marginVertical: 10,
+        // borderWidth: 1,
+        overflow: "hidden",
+        // padding: 5
+    }
+})
--- src/pages/Categorias/index.js
+++ src/pages/Categorias/index.js
@@ -1,14 +1,25 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import CardTitulo from './CardTitulo';
 
 export default function Categorias() {
- return (
-   <View style={{
-    flex: 1,
-    alignItems: 'center', justifyContent: 'center'
-   }}>
-    <Text>
-        Categorias
-    </Text>
-   </View>
+  return (
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ width: '100%', alignItems: 'center' }}>
+          <CardTitulo 
+            imagem="https://bythiti.com.br/wp-content/uploads/2023/09/IMG_8990.jpg"
+            titulo="Canecas"
+          />
+          <CardTitulo 
+            imagem="https://www.lumarepersonalizados.com.br/wp-content/uploads/2021/07/2f1feefb-3ce2-4645-a45b-3d6d65fae158.jpg"
+            titulo="Camisas"
+          />
+          <CardTitulo 
+            imagem="https://d2r9epyceweg5n.cloudfront.net/stores/002/760/771/products/lembrancinhas-chaveiros-personalizados-de-casamento-chaveiros-personalizados1-8cb50dcfeb8c7f482116895377165927-1024-1024.jpg"
+            titulo="Chaveiros"
+          />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
